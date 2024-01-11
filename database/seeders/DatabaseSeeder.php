8ba964b4326d7f2cd1ceacbd36a3ec1eba0b1ee7
Muhamad Renald <avryso@gmail.com>
2024-01-11 21:06:56 +0700

    tes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            // UserSeeder::class,
-            // SoalSeeder::class,
             ProductSeeder::class,
         ]);
     }
